d9d6f5e6b12332a727cc41e9fc49412d2c4178d0
Frank Yournet <yournetf@bxscience.edu>
2025-01-09 11:09:11 -0800

    Added helpers to better understand the scene.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { Canvas } from '@react-three/fiber';
 import './App.css'
 import AnimatedBox from './Components/AnimatedBox';
-import { FirstPersonControls } from '@react-three/drei';
+import { GizmoHelper, GizmoViewcube, OrbitControls } from '@react-three/drei';
 
 function App() {
 
@@ -9,7 +9,12 @@ function App() {
     <>
       <div id='canvas-container'>
         <Canvas style={{backgroundColor: 'deepskyblue'}}>
-          <FirstPersonControls movementSpeed={20} />
+          <GizmoHelper>
+            <GizmoViewcube/>
+          </GizmoHelper>
+          <OrbitControls/>
+          <axesHelper args={[20]}/>
+          <gridHelper args={[100, 50]}/>
           <mesh>
             <torusKnotGeometry args={[1.3, 0.3, 256, 256]}/>
             <meshToonMaterial color={"gold"}/>
