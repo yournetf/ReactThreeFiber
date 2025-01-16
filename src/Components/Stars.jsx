6b75a21d5c333ad22d488250316ff85fda88805d
Frank Yournet <yournetf@bxscience.edu>
2025-01-16 11:50:40 -0800

    Altered stars to create a more rounded star effect.

--- src/Components/Stars.jsx
+++ src/Components/Stars.jsx
@@ -1,11 +1,12 @@
 import { useFrame } from "@react-three/fiber";
-import { useRef } from "react";
+import { useContext, useRef } from "react";
+import { DarkmodeContext } from "../App";
 
 function Star({ position }){
     return(
         <mesh position={position}>
-            <sphereGeometry args={[0.25, 10, 10]}/>
-            <meshBasicMaterial color={0xffffff}/>
+            <sphereGeometry args={[0.25, 10, 10]} />
+            <meshBasicMaterial color={0xffffff} />
         </mesh>
     );
 }
@@ -14,11 +15,17 @@ function Star({ position }){
 function Stars(){
 
     const starComponents = [];
+    const radius = 60;
+    
     for(let i = 0; i < 200; i++){
+        // Random angle.
+        const theta = Math.random() * 2 * Math.PI; 
+        // Random polar angle.
+        const phi = Math.acos((Math.random() * 2) - 1); 
         const positionAry = [
-            ((Math.random() - 0.5) * 100 + 0),
-            ((Math.random() - 0.5) * 100 + 0),
-            ((Math.random() - 0.5) * 100 + 0),
+            radius * Math.sin(phi) * Math.cos(theta),
+            radius * Math.sin(phi) * Math.sin(theta),
+            radius * Math.cos(phi),
         ];
         starComponents.push(<Star key={i} position={positionAry}/>)
     }
@@ -26,14 +33,25 @@ function Stars(){
     const groupRef = useRef();
 
     useFrame(()=>{
-        groupRef.current.rotation.x += 0.0005;
-        groupRef.current.rotation.y += 0.0005;
+        // If Stars aren't renderered, then don't execute.
+        if(groupRef.current){
+            groupRef.current.rotation.x += 0.0005;
+            groupRef.current.rotation.y += 0.0005;
+        }
     })
 
+    const {darkMode, setDarkMode} = useContext(DarkmodeContext);
+
     return(
-        <group ref={groupRef}>
-            {starComponents}
-        </group>
+        <>
+            {darkMode === false ?
+                <></>
+                :
+                <group ref={groupRef}>
+                    {starComponents}
+                </group>
+            }
+        </>         
     );
 }
 export default Stars;
