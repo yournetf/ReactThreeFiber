57126f0c4e811ec5339d52a33b0149117bd73f4c
Frank Yournet <yournetf@bxscience.edu>
2025-02-04 16:39:26 -0800

    Began implementing the new toolBar functionality. Added state change from plus to minus.

--- src/Components/ToolBar.jsx
+++ src/Components/ToolBar.jsx
@@ -2,18 +2,28 @@ import { Svg } from "@react-three/drei";
 import minusSymbol from "/minus-square-svgrepo-com.svg"
 import plusSymbol from "/plus-square-svgrepo-com.svg"
 import { useState } from "react";
+import "../Styles/ToolBar.css";
 
 function ToolBar(){
 
     const [taskBarOpen, setTaskBarOpen] = useState(false);
+
+    const opening = () => {
+        setTaskBarOpen(true);
+    }
+
+    const closing = () => {
+        setTaskBarOpen(false);
+    }
+
     return(
         <>
             
                 {/* ToolBar */}
                 {taskBarOpen ? 
-                    <Svg src={minusSymbol} scale={0.15} position={[0, 0, 50]} />
+                    <Svg src={minusSymbol} scale={0.15} position={[0, 0, 50]} onClick={closing}/>
                         :
-                    <Svg src={plusSymbol} scale={0.15} position={[0, 0, 50]} />
+                    <Svg src={plusSymbol} scale={0.15} position={[0, 0, 50]} onClick={opening} />
                 }
                             
         </>
