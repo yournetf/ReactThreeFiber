992ab484f399c9a79656d2f4b765ff07e733a61d
Frank Yournet <yournetf@bxscience.edu>
2025-02-05 16:33:00 -0800

    Adjusted the toolbar to collapse and expand with github svgs (no animation).

--- src/Components/ToolBar.jsx
+++ src/Components/ToolBar.jsx
@@ -1,32 +1,91 @@
 import { Svg } from "@react-three/drei";
-import minusSymbol from "/minus-square-svgrepo-com.svg"
-import plusSymbol from "/plus-square-svgrepo-com.svg"
 import { useState } from "react";
+import { useCursor } from "@react-three/drei";
+import minusSymbol from "/minus-square-svgrepo-com.svg";
+import plusSymbol from "/plus-square-svgrepo-com.svg";
+import githubSymbol from "/github-svgrepo-com.svg";
 import "../Styles/ToolBar.css";
 
-function ToolBar(){
-
+function ToolBar() {
     const [taskBarOpen, setTaskBarOpen] = useState(false);
+    const [hovered, setHovered] = useState(false);
 
-    const opening = () => {
-        setTaskBarOpen(true);
-    }
+    const opening = () => setTaskBarOpen(true);
+    const closing = () => setTaskBarOpen(false);
 
-    const closing = () => {
-        setTaskBarOpen(false);
-    }
+    useCursor(hovered); // Change cursor when hovering over the GitHub icon
 
-    return(
+    return (
         <>
+            {/* ToolBar */}
+            {taskBarOpen ? (
+                <>
+                <Svg src={minusSymbol} scale={0.15} position={[0, 0, 50]} onClick={closing} onPointerOver={() => setHovered(true)}
+                onPointerOut={() => setHovered(false)}/>
+
+                {/* GitHub Link */}
+                <Svg
+                    src={githubSymbol}
+                    scale={0.15}
+                    position={[6, 0, 50]}
+                    onClick={() => window.open("https://github.com", "_blank")}
+                    onPointerOver={() => setHovered(true)}
+                    onPointerOut={() => setHovered(false)}
+                />
+
+                <Svg
+                    src={githubSymbol}
+                    scale={0.15}
+                    position={[11.5, 0, 50]}
+                    onClick={() => window.open("https://github.com", "_blank")}
+                    onPointerOver={() => setHovered(true)}
+                    onPointerOut={() => setHovered(false)}
+                />
+
+                <Svg
+                    src={githubSymbol}
+                    scale={0.15}
+                    position={[17, 0, 50]}
+                    onClick={() => window.open("https://github.com", "_blank")}
+                    onPointerOver={() => setHovered(true)}
+                    onPointerOut={() => setHovered(false)}
+                />
+
+                <Svg
+                    src={githubSymbol}
+                    scale={0.15}
+                    position={[-6, 0, 50]}
+                    onClick={() => window.open("https://github.com", "_blank")}
+                    onPointerOver={() => setHovered(true)}
+                    onPointerOut={() => setHovered(false)}
+                />
+
+                <Svg
+                    src={githubSymbol}
+                    scale={0.15}
+                    position={[-11.5, 0, 50]}
+                    onClick={() => window.open("https://github.com", "_blank")}
+                    onPointerOver={() => setHovered(true)}
+                    onPointerOut={() => setHovered(false)}
+                />
+
+                <Svg
+                    src={githubSymbol}
+                    scale={0.15}
+                    position={[-17, 0, 50]}
+                    onClick={() => window.open("https://github.com", "_blank")}
+                    onPointerOver={() => setHovered(true)}
+                    onPointerOut={() => setHovered(false)}
+                />
+                </>
+            ) : (
+                <Svg src={plusSymbol} scale={0.15} position={[0, 0, 50]} onClick={opening} onPointerOver={() => setHovered(true)}
+                onPointerOut={() => setHovered(false)}/>
+            )}
+
             
-                {/* ToolBar */}
-                {taskBarOpen ? 
-                    <Svg src={minusSymbol} scale={0.15} position={[0, 0, 50]} onClick={closing}/>
-                        :
-                    <Svg src={plusSymbol} scale={0.15} position={[0, 0, 50]} onClick={opening} />
-                }
-                            
         </>
     );
 }
+
 export default ToolBar;
