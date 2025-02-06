7420473b057ccb956ff9ebdeb591f235d609dedc
Frank Yournet <yournetf@bxscience.edu>
2025-02-06 12:15:45 -0800

    Implemented the icons for the toolbar.

--- src/Components/ToolBar.jsx
+++ src/Components/ToolBar.jsx
@@ -1,9 +1,16 @@
 import { Svg } from "@react-three/drei";
 import { useState } from "react";
 import { useCursor } from "@react-three/drei";
+import { useSpring, animated } from "@react-spring/three";
 import minusSymbol from "/minus-square-svgrepo-com.svg";
 import plusSymbol from "/plus-square-svgrepo-com.svg";
 import githubSymbol from "/github-svgrepo-com.svg";
+import resumeSymbol from "/information-svgrepo-com.svg";
+import certificationSymbol from "/certification-svgrepo-com.svg";
+import appSymbol from "/app-1-svgrepo-com.svg";
+import mailSymbol from "/contact-letter-box-svgrepo-com.svg";
+import linkedInSymbol from "/linkedin-linked-in-svgrepo-com.svg";
+
 import "../Styles/ToolBar.css";
 
 function ToolBar() {
@@ -13,77 +20,98 @@ function ToolBar() {
     const opening = () => setTaskBarOpen(true);
     const closing = () => setTaskBarOpen(false);
 
-    useCursor(hovered); // Change cursor when hovering over the GitHub icon
+    useCursor(hovered);
+
+    // Opacity animation
+    const { opacity } = useSpring({
+        opacity: taskBarOpen ? 1 : 0,
+        config: { duration: 500 },
+    });
 
     return (
         <>
-            {/* ToolBar */}
             {taskBarOpen ? (
                 <>
-                <Svg src={minusSymbol} scale={0.15} position={[0, 0, 50]} onClick={closing} onPointerOver={() => setHovered(true)}
-                onPointerOut={() => setHovered(false)}/>
+                    {/* Minus Symbol */}
+                    <animated.mesh scale={0.15} position={[-2.25, 0, 50]} style={{ opacity }}>
+                        <Svg
+                            src={minusSymbol}
+                            onClick={closing}
+                            onPointerOver={() => setHovered(true)}
+                            onPointerOut={() => setHovered(false)}
+                        />
+                    </animated.mesh>
 
-                {/* GitHub Link */}
-                <Svg
-                    src={githubSymbol}
-                    scale={0.15}
-                    position={[6, 0, 50]}
-                    onClick={() => window.open("https://github.com", "_blank")}
-                    onPointerOver={() => setHovered(true)}
-                    onPointerOut={() => setHovered(false)}
-                />
+                    {/* GitHub Symbol */}
+                    <animated.mesh scale={0.15} position={[3.75, 0, 50]} style={{ opacity }}>
+                        <Svg
+                            src={githubSymbol}
+                            onClick={() => window.open("https://github.com", "_blank")}
+                            onPointerOver={() => setHovered(true)}
+                            onPointerOut={() => setHovered(false)}
+                        />
+                    </animated.mesh>
 
-                <Svg
-                    src={githubSymbol}
-                    scale={0.15}
-                    position={[11.5, 0, 50]}
-                    onClick={() => window.open("https://github.com", "_blank")}
-                    onPointerOver={() => setHovered(true)}
-                    onPointerOut={() => setHovered(false)}
-                />
+                    {/* Resume Symbol */}
+                    <animated.mesh scale={0.08} position={[9.75, 0, 50]} style={{ opacity }}>
+                        <Svg
+                            src={resumeSymbol}
+                            onClick={() => window.open("/Frank_Yournet_Resume.pdf", "_blank")}
+                            onPointerOver={() => setHovered(true)}
+                            onPointerOut={() => setHovered(false)}
+                        />
+                    </animated.mesh>
 
-                <Svg
-                    src={githubSymbol}
-                    scale={0.15}
-                    position={[17, 0, 50]}
-                    onClick={() => window.open("https://github.com", "_blank")}
-                    onPointerOver={() => setHovered(true)}
-                    onPointerOut={() => setHovered(false)}
-                />
+                    {/* Certification Symbol */}
+                    <animated.mesh scale={0.2} position={[15.25, 0, 50]} style={{ opacity }}>
+                        <Svg
+                            src={certificationSymbol}
+                            onClick={() => window.open("AWS_Course_Completion_Certificate.pdf", "_blank")}
+                            onPointerOver={() => setHovered(true)}
+                            onPointerOut={() => setHovered(false)}
+                        />
+                    </animated.mesh>
 
-                <Svg
-                    src={githubSymbol}
-                    scale={0.15}
-                    position={[-6, 0, 50]}
-                    onClick={() => window.open("https://github.com", "_blank")}
-                    onPointerOver={() => setHovered(true)}
-                    onPointerOut={() => setHovered(false)}
-                />
+                    {/* LinkedIn Symbol */}
+                    <animated.mesh scale={0.15} position={[-8.25, 0, 50]} style={{ opacity }}>
+                        <Svg
+                            src={linkedInSymbol}
+                            onClick={() => window.open("https://linkedin.com", "_blank")}
+                            onPointerOver={() => setHovered(true)}
+                            onPointerOut={() => setHovered(false)}
+                        />
+                    </animated.mesh>
 
-                <Svg
-                    src={githubSymbol}
-                    scale={0.15}
-                    position={[-11.5, 0, 50]}
-                    onClick={() => window.open("https://github.com", "_blank")}
-                    onPointerOver={() => setHovered(true)}
-                    onPointerOut={() => setHovered(false)}
-                />
+                    {/* App Symbol */}
+                    <animated.mesh scale={0.005} position={[-13.75, 0, 50]} style={{ opacity }}>
+                        <Svg
+                            src={appSymbol}
+                            onClick={() => window.open("https://github.com", "_blank")}
+                            onPointerOver={() => setHovered(true)}
+                            onPointerOut={() => setHovered(false)}
+                        />
+                    </animated.mesh>
 
-                <Svg
-                    src={githubSymbol}
-                    scale={0.15}
-                    position={[-17, 0, 50]}
-                    onClick={() => window.open("https://github.com", "_blank")}
-                    onPointerOver={() => setHovered(true)}
-                    onPointerOut={() => setHovered(false)}
-                />
+                    {/* Mail Symbol */}
+                    <animated.mesh scale={0.08} position={[-19.25, 0, 50]} style={{ opacity }}>
+                        <Svg
+                            src={mailSymbol}
+                            onClick={() => window.open("mailto:example@email.com")}
+                            onPointerOver={() => setHovered(true)}
+                            onPointerOut={() => setHovered(false)}
+                        />
+                    </animated.mesh>
                 </>
             ) : (
-                <Svg src={plusSymbol} scale={0.15} position={[0, 0, 50]} onClick={opening} onPointerOver={() => setHovered(true)}
-                onPointerOut={() => setHovered(false)}/>
+                <Svg
+                    src={plusSymbol}
+                    scale={0.15}
+                    position={[-2.25, 0, 50]}
+                    onClick={opening}
+                    onPointerOver={() => setHovered(true)}
+                    onPointerOut={() => setHovered(false)}
+                />
             )}
-
-            
         </>
     );
 }
